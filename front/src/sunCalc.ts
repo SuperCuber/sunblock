70b31109e963cb53d6b932053d3d77e746320c47
SuperCuber <amit.gold00@gmail.com>
2022-07-14 20:49:51 +0300

    Improve angle calculations

--- front/src/sunCalc.ts
+++ front/src/sunCalc.ts
@@ -22,22 +22,8 @@ export function optimizeRoute(shape: LatLngLiteral[], date: Date, position: LatL
     let a = shape[i - 1]
     let b = shape[i]
     let direction_vec = [b.lng - a.lng, b.lat - a.lat]
-    // Angle between direction_vec and east
-    let direction_theta = Math.acos(
-      direction_vec[0]
-      / Math.sqrt(direction_vec[0] * direction_vec[0]
-        + direction_vec[1] * direction_vec[1]
-      )
-    )
-    if (direction_vec[1] < 0) {
-      // In case of south, convert clockwise angle to anticlockwise "big" angle
-      direction_theta = 2 * Math.PI - direction_theta
-    }
-
+    let direction_theta = Math.atan2(direction_vec[1], direction_vec[0])
     let angle_with_sun = direction_theta - azimuth
-    if (angle_with_sun > 2 * Math.PI) {
-      angle_with_sun -= 2 * Math.PI
-    }
 
     parts.push({ p: [a, b], angle: angle_with_sun })
   }
